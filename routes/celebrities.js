abb8784ecc8e29df9812057b7ee61a626244f370
Miguel Sanchez <migsandia@gmail.com>
2019-03-03 23:17:34 +0100

    iteration 4 succes

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -12,6 +12,24 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
+router.get('/new', (req, res, next) => {
+  res.render('celebrities/new')
+})
+
+router.post('/', async (req, res, next) => {
+  const { _id, name, occupation, catchPhrase } = req.body
+  const celebrity = { name, occupation, catchPhrase }
+  try {
+    if (_id) {
+      await Celebrity.findByIdAndUpdate(_id, celebrity)
+    } else {
+      await Celebrity.create(celebrity)
+    }
+    res.redirect('/celebrities')
+  } catch (error) {
+    res.redirect('/celebrities/new')
+  }
+})
 router.get('/:id', async (req, res, next) => {
   const { id } = req.params
   try {
